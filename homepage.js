a2fde6e296a7ace5f0a88437767d874a598018fc
Hinson1Cheung <1155175864@link.cuhk.edu.hk>
2024-03-25 22:35:45 +0800

    add animation

--- homepage.js
+++ homepage.js
@@ -5,3 +5,10 @@ $(document).ready(function(){
         return false;
     });
 });
+$(document).ready(function() {
+    $('.product-item').hover(function() {
+        $(this).find('.product-button').css({'visibility': 'visible', 'opacity': 1});
+    }, function() {
+        $(this).find('.product-button').css({'visibility': 'hidden', 'opacity': 0});
+    });
+});
