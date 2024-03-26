ce8f285c273fbbb2ddaeddb743a8dbb92a216ce9
Hinson1Cheung <1155175864@link.cuhk.edu.hk>
2024-03-26 11:58:53 +0800

    make animation smooth

--- Cart/cart.js
+++ Cart/cart.js
@@ -3,3 +3,23 @@ window.onload = function() {
     var link = document.getElementById('historyLink');
     link.textContent = referrer;
 }
+window.onload = function() {
+    var referrer = "Pervious Page";
+    var link = document.getElementById('historyLink');
+    link.textContent = referrer;
+    link.onclick = function() {
+        if (window.location.href.includes('#')) {
+            history.go(-2);
+        } else {
+            history.go(-1);
+        }
+        return false;
+    }
+}
+$(document).ready(function(){
+    $('.scroll-to-top').click(function(event){
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, 'slow');
+        return false;
+    });
+});
--- StoreManagement/StoreManagement.js
+++ StoreManagement/StoreManagement.js
@@ -10,3 +10,16 @@ window.onload = function() {
     var link = document.getElementById('historyLink');
     link.textContent = referrer;
 }
+window.onload = function() {
+    var referrer = "Pervious Page";
+    var link = document.getElementById('historyLink');
+    link.textContent = referrer;
+    link.onclick = function() {
+        if (window.location.href.includes('#')) {
+            history.go(-2);
+        } else {
+            history.go(-1);
+        }
+        return false;
+    }
+}
