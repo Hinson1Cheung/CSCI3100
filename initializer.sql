e6a53d40809a383ed9efead122f078bd2e145769
JohnLeiShingHon <john.shinghonlei@gmail.com>
2024-04-08 00:21:58 +0800

    Added sql statements for creating the server, subject to change

--- initializer.sql
+++ initializer.sql
@@ -5,25 +5,42 @@ set global local_infile = true;
 create table if not exists users(
     UName varchar(256) unique not null,
     PWord varchar(256) not null,
-    UID int(20) primary key auto_increment
+    UID int(10) primary key auto_increment,
+    balance float not null default 0
 );
 
 create table if not exists admins(
     AName varchar(256) unique not null,
     Pword varchar(256) not null,
-    AID int(20) primary key auto_increment
+    AID int(10) primary key auto_increment
 );
 
 create table if not exists product(
-    productID int(20) primary key auto_increment,
+    productID int(15) primary key auto_increment,
     pName varchar(256) not null,
     imageURL varchar(256), 
-    rating int(20), 
+    rating int, 
+    quantity int not null default 0,
     category varchar(256) not null,
     description text, 
     price float not null
 );
 
 create table if not exists ShopCart(
-    
+    cartID int(10) primary key auto_increment,
+    count int not null,
+    UID int(10) not null references user(UID),
+    productID int(50) not null references product(productID)
+);
+
+create table if not exists category(
+    catID int(5) primary key auto_increment, 
+    catName varchar(256) not null
+);
+
+create table if not exists transaction (
+    transID int(30) primary key auto_increment, 
+    productID int(15) not null references product(productID), 
+    UID int(10) not null references user(UID), 
+    sum int not null, 
 );
