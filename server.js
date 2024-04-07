64210aeba87e52d6f6341e267c0953beb366f191
Hinson1Cheung <1155175864@link.cuhk.edu.hk>
2024-04-07 23:53:25 +0800

    make some change of server.js

--- server.js
+++ server.js
@@ -2,3 +2,12 @@ const express = require("express");
 const app = express();
 const homePage = require("./Frontend/index.html");
 const mysql = require("mysql2");
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './Frontend/index.html'));
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
