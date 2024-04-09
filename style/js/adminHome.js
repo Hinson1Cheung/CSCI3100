f39f08ab61caadc4a6a67c3d0a8d89720337ef75
Hinson1Cheung <1155175864@link.cuhk.edu.hk>
2024-04-10 02:24:30 +0800

    fix the search bar at admin page

--- style/js/adminHome.js
+++ style/js/adminHome.js
@@ -5,23 +5,3 @@ $(document).ready(function() {
         $(this).find('.product-button').css({'visibility': 'hidden', 'opacity': 0});
     });
 });
-$(document).ready(function(){
-    $('.scroll-to-top').click(function(event){
-        event.preventDefault();
-        $('html, body').animate({scrollTop: 0}, 'slow');
-        return false;
-    });
-});
-window.onload = function() {
-    var referrer = "Pervious Page";
-    var link = document.getElementById('historyLink');
-    link.textContent = referrer;
-    link.onclick = function() {
-        if (window.location.href.includes('#')) {
-            history.go(-2);
-        } else {
-            history.go(-1);
-        }
-        return false;
-    }
-}
