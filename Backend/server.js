66d989d030de5d03ef0e6d6b30fd88f8a0a1288d
JohnLeiShingHon <john.shinghonlei@gmail.com>
2024-05-04 17:31:24 +0800

    session info

--- Backend/server.js
+++ Backend/server.js
@@ -14,6 +14,7 @@ app.use(uploader());
 
 
 //const homePage = require("./Frontend/index.html");
+//session info:https://expressjs.com/en/resources/middleware/session.html
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 
